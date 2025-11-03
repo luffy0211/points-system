4fd815eb655c3844a3f5ca75002922a11873b959
luffy0211 <15139442846@163.com>
2025-11-03 23:22:17 +0800

    修改积分冷却时间为1小时

--- js/config.js
+++ js/config.js
@@ -7,6 +7,6 @@ const SUPABASE_CONFIG = {
 // 系统配置
 const SYSTEM_CONFIG = {
     imageReviewTimeLimit: 24,  // 图片审核时间限制（小时）
-    redeemCooldown: 48,        // 兑换冷却时间（小时）
+    redeemCooldown: 1,        // 兑换冷却时间（小时）
     pointsPerUpload: 10        // 每次上传获得的积分
 };
